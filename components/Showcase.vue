cc79ed8650ba0a6eaf2ff1eebfc49ef54f2cd80d
tadashi-aikawa <syou.maman@gmail.com>
2026-05-16 22:20:35 +0900

    Showcaseコンポーネントを追加

---
+++ components/Showcase.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  sizeClass: string;
+  gapClass: string;
+  sources: {
+    name: string;
+    url: string;
+  }[];
+}>();
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <div
+      v-for="source in sources"
+      :key="source.name"
+      class="flex flex-col items-center"
+      :class="gapClass"
+    >
+      <img :src="source.url" :alt="source.name" :class="sizeClass" />
+      {{ source.name }}
+    </div>
+  </div>
+</template>
